2f017af5554785170fd54a942051ccc2dccc78f3
camilo <jumellecamilo@gmail.com>
2023-11-24 13:02:32 +0100

    Added nodes and edited index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import logger from 'morgan';
 
+import { Server } from "socket.io";
+import {createServer} from 'node:http';
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connectuon',() => {
+    console.log('a user has connected');
+})
 app.use(logger('dev'))
 
 
@@ -11,6 +20,6 @@ app.get('/',(req, res)=> {
     res.sendFile(process.cwd() + '/src/views/index.html')
 })
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server running on port ${port}`);
 })
